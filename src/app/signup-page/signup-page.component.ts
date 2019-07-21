57f2cfb1802f376c6520617e5b2760443df94925
magelon <alfardproject@gmail.com>
2019-07-20 22:01:55 -0700

    c

--- src/app/signup-page/signup-page.component.ts
+++ src/app/signup-page/signup-page.component.ts
@@ -1,7 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute  } from '@angular/router';
 import { FormBuilder } from '@angular/forms';
- 
+
+import { AngularFireDatabase,AngularFireObject  } from '@angular/fire/database';
+import { Observable } from 'rxjs';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { auth } from 'firebase/app';
 
 @Component({
   selector: 'app-signup-page',
@@ -12,10 +16,17 @@ export class SignupPageComponent implements OnInit {
 public role;
 public truckerDiv=false;
 
+public alreadySignUp=false;
+
+itemRef: AngularFireObject<any>;
+item: Observable<any>;
+
 public title;
 public checkoutForm;
   constructor( private route: ActivatedRoute,
     private formBuilder: FormBuilder,
+    public db: AngularFireDatabase,
+    public afAuth:AngularFireAuth
     ) {
 
       this.checkoutForm = this.formBuilder.group({
@@ -37,8 +48,41 @@ public checkoutForm;
 
      onSubmit(customerData) {
       // Process checkout data here
-      console.warn('Your order has been submitted', customerData);
-   
+      
+      console.warn('Your order has been submitted', customerData.CompanyName);
+
+if(this.afAuth.user){
+  let path=`${this.role}/${this.afAuth.auth.currentUser.uid}`;
+
+  if(this.role=="trucker"){
+    let data={
+      DriverName: customerData.DriverName,
+      Employer:customerData.Employer,
+      Phone:customerData.Phone,
+      Email:customerData.Email,
+      Address:customerData.Address,
+    }
+    this.db.object(path).update(data).catch(error=>console.log(error));
+  }else if(this.role=="broker"){
+    let data={
+      CompanyName: customerData.CompanyName,
+      DispatcherName: customerData.DispatcherName,
+      CompanyPhone: customerData.CompanyPhone,
+      CompanyEmail:customerData.CompanyEmail,
+      CompanyAddress:customerData.CompanyAddress,
+    }
+    this.db.object(path).update(data).catch(error=>console.log(error));
+  }else if(this.role=="owner"){
+    let data={
+      CompanyName: customerData.CompanyName,
+      DispatcherName: customerData.DispatcherName,
+      CompanyPhone: customerData.CompanyPhone,
+      CompanyEmail:customerData.CompanyEmail,
+      CompanyAddress:customerData.CompanyAddress,
+    }
+    this.db.object(path).update(data).catch(error=>console.log(error));
+  }
+}
       this.checkoutForm.reset();
     }
 
@@ -72,6 +116,24 @@ if(this.role=="trucker"){
 }else{
   this.truckerDiv=false;
 }
+
+this.itemRef = this.db.object(`${this.role}/${this.afAuth.auth.currentUser.uid}`);
+this.itemRef.snapshotChanges().subscribe(action => {
+  console.log(action.type);
+  console.log(action.key)
+  console.log(action.payload.val())
+  if(action.payload.val()==null){
+    this.alreadySignUp=false;
+  }else{
+    this.alreadySignUp=true;
+  }
+});
+
+
+//
+console.log(this.item);
+
+
 
   }
 
